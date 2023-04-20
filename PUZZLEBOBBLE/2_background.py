3e97acf3cbd9faa42c6b0d90384a04de9cf291b1
Jaehyun_Park <115146172+vgb30xk@users.noreply.github.com>
2023-04-20 14:39:36 +0900

    4/20

---
+++ PUZZLEBOBBLE/2_background.py
@@ -0,0 +1,28 @@
+# 배경 이미지 설정
+import pygame
+import os
+
+pygame.init()
+screen_width = 448
+screen_height = 720
+screen = pygame.display.set_mode((screen_width, screen_height))
+pygame.display.set_caption("PUZZLE BOBBLE")
+clock = pygame.time.Clock()
+
+# 배경 이미지 불러오기
+current_path = os.path.dirname(__file__)
+background = pygame.image.load(os.path.join(current_path, "background.png"))
+
+
+running = True
+while running:
+    clock.tick(60)  # FPS 60으로 설정
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    screen.blit(background, (0, 0))
+    pygame.display.update()
+
+pygame.quit()
